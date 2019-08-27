3236af6e7ec50cf4fde93ca6ddc74c3b961f7250
One Punch <bodnguye@gmail.com>
2019-08-27 15:04:08 -0700

    banner

---
+++ build/precache-manifest.b8cb0fbeb35c21b8f9e6a2c2f6735949.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2245425a934c24a8453020237987b8a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "311b2402f1d1af79cf14",
+    "url": "/static/css/main.663fe51a.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "311b2402f1d1af79cf14",
+    "url": "/static/js/main.0327df39.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
